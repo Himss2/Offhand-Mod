8a8d961d74ef0c8b83d25dc3a024912e03427f78
Fathul <97374618+Himss2@users.noreply.github.com>
2026-09-14 17:06:27 +0700

    chore: package v0.2.63 levipack

--- scripts/build.sh
+++ scripts/build.sh
@@ -98,7 +98,7 @@ DIST_DIR="$ROOT/dist/$ABI"
 
 PACKAGE_DIR="$DIST_DIR/levi-offhand"
 
-LEVIPACK="$DIST_DIR/levi-offhand-v0.2.62.levipack"
+LEVIPACK="$DIST_DIR/levi-offhand-v0.2.63.levipack"
 
 
 echo "Using Android NDK: $NDK"
