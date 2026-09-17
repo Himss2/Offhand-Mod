7a2d823b4dc92fdeb4f8391c2dfef559dc3ab606
Fathul <97374618+Himss2@users.noreply.github.com>
2026-09-17 19:55:48 +0700

    fix: route 1.26.51.1 native right-use hand

--- src/runtime/RightUseRouter.hpp
+++ src/runtime/RightUseRouter.hpp
@@ -30,7 +30,18 @@ private:
     static bool baseUseItemDetour(
         void* gameMode,
         const void* itemStack,
-        unsigned char useContext
+        unsigned char hand
+    ) noexcept;
+
+    static std::uint32_t useItemOnBlockDetour(
+        void* gameMode,
+        const void* context,
+        const void* blockPos,
+        int face,
+        const void* hitPos,
+        unsigned char hand,
+        std::uintptr_t extra,
+        bool flag
     ) noexcept;
 
     static const void* selectedItemDetour(const void* player) noexcept;
@@ -41,17 +52,21 @@ private:
     std::unique_ptr<pl::memory::HookHandle> mSelectedItemHook;
     std::unique_ptr<pl::memory::HookHandle> mReleaseUsingItemHook;
     std::unique_ptr<pl::memory::HookHandle> mBaseUseItemHook;
+    std::unique_ptr<pl::memory::HookHandle> mUseItemOnBlockHook;
 
     void* mSelectedItemOriginal{nullptr};
     void* mReleaseUsingItemOriginal{nullptr};
     void* mBaseUseItemOriginal{nullptr};
+    void* mUseItemOnBlockOriginal{nullptr};
 
     std::uintptr_t mSelectedItemTarget{0};
     std::uintptr_t mReleaseUsingItemTarget{0};
     std::uintptr_t mBaseUseItemTarget{0};
+    std::uintptr_t mUseItemOnBlockTarget{0};
 
     std::atomic_bool mFeatureEnabled{false};
     std::atomic_bool mLoggedOffhandUse{false};
+    std::atomic_bool mLoggedBlockUse{false};
     std::atomic_bool mLoggedLongUse{false};
 };
 
