5e92246f9aa7b2754067a2ea294b23e4f3a7db3a
Fathul <97374618+Himss2@users.noreply.github.com>
2026-09-19 15:17:40 +0700

    restore: proven snapshot-based offhand placement header

--- src/runtime/RightUseRouter.hpp
+++ src/runtime/RightUseRouter.hpp
@@ -67,7 +67,6 @@ private:
     std::atomic_bool mFeatureEnabled{false};
     std::atomic_bool mLoggedOffhandUse{false};
     std::atomic_bool mLoggedBlockUse{false};
-    std::atomic_bool mLoggedAttackOnlyYield{false};
     std::atomic_bool mLoggedLongUse{false};
 };
 
